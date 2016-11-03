8e9170ebbbb5274d7a8b5c84339fe5442c9bf15d
Samukeras <felicio.samuel@gmail.com>
2016-11-02 23:11:02 -0200

    Criação das tabelas

--- app/src/main/java/udesc/br/rakesfoot/core/seeder/SeederGame.java
+++ app/src/main/java/udesc/br/rakesfoot/core/seeder/SeederGame.java
@@ -1,9 +1,18 @@
 package udesc.br.rakesfoot.core.seeder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import udesc.br.rakesfoot.core.persistence.Persistible;
 import udesc.br.rakesfoot.core.util.connection.Connection;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoChampionship;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoEvent;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoManager;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoMatch;
 import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoPlayer;
 import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoSeason;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoStadium;
+import udesc.br.rakesfoot.game.model.dao.sqlite.SqliteDaoTeam;
 
 /**
  * Created by felic on 01/11/2016.
@@ -16,12 +25,18 @@ public class SeederGame implements Seeder {
 
     @Override
     public void seed(Connection connection) {
-        SqliteDaoSeason sqliteDaoSeason = new SqliteDaoSeason(connection.getContext(), Connection.INITIAL_VERSION);
-//        Persistible season = new SqliteDaoSeason(connection.getContext(), Connection.INITIAL_VERSION),
-//                    player = new SqliteDaoPlayer(connection.getContext(), Connection.INITIAL_VERSION);
+        List<Persistible> seeds = new ArrayList<>();
+        seeds.add(new SqliteDaoSeason(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoChampionship(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoEvent(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoManager(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoMatch(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoPlayer(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoStadium(connection.getContext(), Connection.INITIAL_VERSION));
+        seeds.add(new SqliteDaoTeam(connection.getContext(), Connection.INITIAL_VERSION));
 
-        sqliteDaoSeason.onCreate();
-//        season.onCreate();
-//        player.onCreate();
+        for(Persistible seed : seeds) {
+            seed.onCreate();
+        }
     }
 }
